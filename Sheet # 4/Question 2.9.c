41240ca70ff1b70326f14c746fa0ccd31ff1eba4
Mr.Shadow <97762724+Shadowlo21@users.noreply.github.com>
2024-11-22 16:01:17 +0200

    Add files via upload

---
+++ Sheet # 4/Question 2.9.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main() {
+	int grade;
+	printf("Enter Ur Grade : ");
+	scanf("%d",&grade);
+    if (grade < 1 || grade > 100)
+    {
+        printf("Enter a valid grade \n");
+        return 0;
+    }
+    switch (grade /10) {
+     case 10:
+     case 9:
+     case 8:
+        printf("Excellent\n");
+        break;
+    case 7:
+        printf("Very Good\n");
+        break;
+    case 6:
+        printf("Good\n");
+        break;
+    case 5:
+        printf("Pass\n");
+        break;
+    default:
+        printf("Fail\n");
+        break;
+    }
+    return 0;
+}
+
