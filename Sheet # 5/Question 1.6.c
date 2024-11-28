5bcc9a49aecea0929bb07be979d93995c622f73e
Mr.Shadow <97762724+Shadowlo21@users.noreply.github.com>
2024-11-28 19:04:02 +0200

    Update Question 1.6.c

--- Sheet # 5/Question 1.6.c
+++ Sheet # 5/Question 1.6.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-
-
 int main() {
-	int num; 
+	int num = 0 ,i = 0 , sum = 0;
 	do
 	{
 		printf("Enter positive numbers (enter a negative number to stop):  ");
 		scanf("%d", &num);
 		if (num < 0)
 		{
-			printf("\nLoop Down ");
 			break;
 		}
+		else {
+			sum += num;
+			i++;
+		}
 	} while (1);
+	printf("The average of the positive numbers is: %f\n", ((float)sum / (float)i));
 }
